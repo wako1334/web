9f327f0f664c67ef260c63446e45dadc8927fc6d
wako1334 <jorge.carvallo@optimissa.com>
2022-02-10 12:05:22 -0600

    ttt

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/index', (req, res) => res.send('Hello World-index!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
